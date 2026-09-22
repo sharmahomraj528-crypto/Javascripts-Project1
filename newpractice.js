2679a0cc49779218aa90cc880ae41761d4377d9e
HOMRAJ SHARMA <sharmahomraj528@example.com>
2026-09-22 21:37:14 +0530

    practice

---
+++ newpractice.js
@@ -0,0 +1,21 @@
+var studentname= "Rahul"
+let age=19;
+var course = "Bsc datascience";
+var college="Sitm"
+let marks=59;
+const birthYear = 2007;
+let city="Guwahati"
+var semester=1;
+let favouritesubject ="Javascript"
+
+
+console.log("studentname:",studentname);
+console.log("age:", age);
+console.log("course:", course);
+console.log("college:",college);
+console.log(marks);
+console.log("birthYear:",birthYear);
+console.log("city:",city);
+console.log("semester:",semester);
+console.log("favouritesubject:",favouritesubject);
+
